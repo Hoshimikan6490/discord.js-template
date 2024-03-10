3e9383f0de3548a9f326b6ce4bdc49e659064428
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-03-10 11:46:18 +0900

    公開用に調整

---
+++ commands/simple_response.js
@@ -0,0 +1,26 @@
+module.exports = {
+  data: {
+    name: "simple_response",
+    description: "このコードの書き方で最も簡単なコマンドです！",
+  },
+  async execute(interaction) {
+    await interaction.reply({
+      embeds: [
+        {
+          title: "こんにちは！",
+          description:
+            "このメッセージが送信されていれば、正常にスラッシュコマンドの設定ができています！",
+          thumbnail: {
+            url: "attachment://image.png",
+          },
+        },
+      ],
+      files: [
+        {
+          attachment: "images/ojigi.png",
+          name: "image.png",
+        },
+      ],
+    });
+  },
+};
--- index.js
+++ index.js
@@ -1,77 +1,110 @@
-const http = require('http');
-http.createServer(function(req, res) {
-  res.write('Discord bot is active.\nPleace check it.');
-  res.end();
-}).listen(8000);
+//パッケージ系の定義
+const fs = require("fs");
+const {
+  Client,
+  GatewayIntentBits,
+  InteractionType,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+require("dotenv").config();
 
-
-const fs = require('fs')
-const { Client, GatewayIntentBits, Partials, InteractionType, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField } = require('discord.js');
-
+//Discordのclientを設定
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
 });
 
-const commands = {}
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+//コマンドを「/commands」フォルダから呼び出して、commands変数に代入
+const commands = {};
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-  commands[command.data.name] = command
+  commands[command.data.name] = command;
 }
 
+//Discord BOTを起動したときに動作する内容を設定
 client.once("ready", async () => {
-  const data = []
+  //先ほど読み込んだコマンドをBOTに反映
+  const data = [];
   for (const commandName in commands) {
-    data.push(commands[commandName].data)
+    data.push(commands[commandName].data);
   }
   await client.application.commands.set(data);
+
+  //ログインできたことをターミナルに表示
   console.log("Ready!");
+
+  //「○○をプレイ中！！」みたいなやつを設定。この場合は10秒に１回更新する。
   setInterval(() => {
     client.user.setActivity({
       name: `所属サーバー数は、${client.guilds.cache.size}サーバー｜Ping値は、${client.ws.ping}ms｜replitで起動中です`,
     });
   }, 10000);
-  client.channels.cache.get('889486664760721418').send('起動しました！');
+
+  //起動したことを、開発ログ用のチャンネルに送信
+  client.channels.cache.get("889486664760721418").send("起動しました！");
 });
 
+//interactionが発行された時に何をするかを設定
 client.on("interactionCreate", async (interaction) => {
-    if (interaction.customId == "test") {
-    interaction.reply("GOOOOOOOOOOOOOD!")
+  //カスタムIDが「test」の物(ボタン)が実行(押された)時に
+  if (interaction.customId == "test") {
+    //それに返信をする
+    interaction.reply("GOOOOOOOOOOOOOD!");
   }
-  
-  if (!interaction.type === InteractionType.ApplicationCommand) {
-    return;
-  }
-  const command = commands[interaction.commandName];
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: '内部エラーが発生しました。管理者にお問い合わせください。',
-      ephemeral: true,
-    })
+
+  //スラッシュコマンドが実行された時に
+  if (interaction.type === InteractionType.ApplicationCommand) {
+    //「commands」フォルダの中のどのファイルにinteractionの情報を送るかを決めるために、そのinteractionコマンドのコマンド名を定義したファイルに送信する。(つまりファイル名は関係ない)
+    const command = commands[interaction.commandName];
+
+    try {
+      //コマンドを定義したファイルにinteractionの情報を送る
+      await command.execute(interaction);
+
+      //エラーが発生した場合は
+    } catch (error) {
+      //ターミナルにエラー内容を表示する
+      console.error(error);
+      //ユーザーには、内部エラーが発生した旨のみを伝える。
+      await interaction.reply({
+        content: "内部エラーが発生しました。管理者にお問い合わせください。",
+        ephemeral: true,
+      });
+    }
   }
 });
 
 ///////////////////////////////////////////////////////////////////////////////
 
-client.on("messageCreate", async message => {
-  if (message.content === "mca!test") {
-    const button = new ActionRowBuilder()
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId('test')
-          .setLabel('TEST')
-          .setStyle(ButtonStyle.Primary)
-          .setEmoji("💞")
-        );
+//何らかのメッセージを受信した場合は
+client.on("messageCreate", async (message) => {
+  //メッセージの内容が「$test」だった時に
+  if (message.content === "$test") {
+    //ボタンを準備する
+    const button = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("test")
+        .setLabel("TEST")
+        .setStyle(ButtonStyle.Primary)
+        .setEmoji("💤")
+    );
+
+    //メッセージを受信したチャンネルにメッセージを送信する。
     message.channel.send({
       content: "TEST!!!!!!!!!!",
-      components: [button]
+      components: [button],
     });
   }
 });
 
+//Discord BOTにログイン
 client.login(process.env.TOKEN);
